f1ff6e16b23b735b2434ff706de83421a63a2321
alinhai <2634320999@qq.com>
2023-08-15 22:32:14 +0800

    feat: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // build: {
-  //   assetsInlineLimit: Infinity
-  // },
+  build: {
+    // assetsInlineLimit: Infinity
+    outDir: 'docs'
+  },
   plugins: [react()],
 })
